bc67128c7e027041cacca2f2b7c7df90eababcec
Humayun kabir <humayunkabir.cep@gmail.com>
2020-03-22 10:41:27 +0600

    Task Model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  all(): any[] {
+  all(): Task[] {
     return this.taskService.all();
   }
 }
